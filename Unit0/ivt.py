1a011a2f01bc616d04d83e6be207098f7c0f4c59
Jeffrey Elkner <jeff@elkner.net>
2017-06-18 15:32:59 -0400

    Added quotient limit functions

--- Unit0/ivt.py
+++ Unit0/ivt.py
@@ -7,3 +7,15 @@ def f(x):
 
 def g(x):
     return 4 * x ** -3 - x - sin(math.pi * x)
+
+
+def ql3(x):
+    return ((12 / x) - 3 * x) / (2 - 3 * x + x ** 2)
+
+
+def ql4(x):
+    return (2 * x **2 - 10 * x + 12) / (x ** 3 - 6 * x ** 2 + 9 * x)
+
+
+def ql5(x):
+    return (3 * x ** 3 + x ** 2) / (x ** 3 + x ** 2 + x)
