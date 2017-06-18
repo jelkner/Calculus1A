77fc12991a466fbab58c371ae20d9b3d14294c52
Jeffrey Elkner <jeff@elkner.net>
2017-06-18 12:57:26 -0400

    Added functions g, h, and j

--- Unit0/closer.py
+++ Unit0/closer.py
@@ -1,2 +1,17 @@
+import math
+
+
 def f(x):
     return (3 - 5 * x + x ** 2 + x ** 3) ** 0.5 / (x - 1)
+
+
+def g(x):
+    return x / math.tan(2 * x)
+
+
+def h(x):
+    return (abs(x) + math.sin(x)) / x ** 2
+
+
+def j(x):
+    return math.sin(13 / x)
